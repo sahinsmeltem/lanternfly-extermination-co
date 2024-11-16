a79306a87431cf8a44d080ec4c59d8fc6f409a80
Meltem Sahin <meltemsahin@meltems-mbp.wifi.local.cmu.edu>
2024-11-15 21:20:34 -0500

    first act!

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 print('Lanternfly Extermination Co.')
 print('hello')
 
-from cmu_graphics import *
+from cmu_graphics import * 
+
+def onAppStart(app):
+    app.width = 400
+    app.height = 600
+
+
+def redrawAll(app):
+    drawRect(100, 100, 100, 100, fill = 'red')
+
+def main():
+    runApp()
+main()
