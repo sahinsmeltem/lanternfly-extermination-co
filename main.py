5d9c899e4541635692eb443ae3fc07bfc344fce5
Meltem Sahin <meltemsahin@meltems-mbp.wifi.local.cmu.edu>
2024-11-15 21:51:40 -0500

    v2

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
 print('Lanternfly Extermination Co.')
-print('hello')
 
 from cmu_graphics import * 
+import random
 
 def onAppStart(app):
+    app.flies = [ ]
     app.width = 400
     app.height = 600
+    generate
 
+
+def onStep(app):
+    pass
+
+
+def onMousePress(app, mouseX, mouseY):
+    r = random.randrange(5, 50)
+    color = random.choice(['red', 'orange', 'yellow', 'green', 'blue', 'purple'])
+    dot = makeDot(mouseX, mouseY, r, color)
+    app.dots.append(dot)
+    print('Added:', dot)
+    
 
 def redrawAll(app):
-    drawRect(100, 100, 100, 100, fill = 'red')
+
+
+class Fly:
+    def __init__(self, cx, cy, size, bTime):
+        self.cx = cx
+        self.cy = cy
+        self.size = size
+        self.color = 'black'
+        return dot
+
+    
+    def draw(self):
+        
+
+
 
 def main():
     runApp()
 main()
+
